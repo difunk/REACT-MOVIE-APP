5c584f97450240a9ce7e953dd275ced2e07cf081
difunk <so0mewhat2@gmail.com>
2025-04-02 09:41:35 +0200

    edit App.jsx: debounce search term, encode query parameters to ensure URL safety add component for movie-cards & search functionality

--- react-movie-app/src/App.jsx
+++ react-movie-app/src/App.jsx
@@ -1,7 +1,10 @@
-import { useState, useEffect, use } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 import Search from './components/Search'
 import Spinner from './components/Spinner';
+import MovieCard from './components/MovieCard';
+import { useDebounce } from 'react-use';
+import { getTrendingMovies, updateSearchCount } from './appwrite';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -15,20 +18,27 @@ const API_OPTIONS = {
   }
 }
 
-
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("")
   const [errorMessage, setErrorMessage] = useState("")
   const [movieList, setMovieList] = useState([])
+  const [trendingMovies, setTrendingMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true)
+  const [debouncedSearchTerm , setDebouncedSearchTerm ] = useState('')
+  const allMoviesRef = useRef(null);
 
-  const fetchMovies = async() => {
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
+
+  const fetchMovies = async(query = '') => {
     setIsLoading(true);
-    setErrorMessage(false);
+    setErrorMessage('');
     try{
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query 
+      ?  `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      :  `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+     
       const response = await fetch(endpoint, API_OPTIONS);
-
+      
       if(!response.ok){
         throw new Error('Failed to fetch movies')
       }
@@ -43,15 +53,41 @@ const App = () => {
 
       setMovieList(data.results || []);
 
+      if(query && data.results.length > 0 ){
+        await updateSearchCount(query, data.results[0])
+        scrollToAllMovies();
+      }
+
     } catch(error) {
       console.log(`Error fetching movies: ${error}`)
+      setErrorMessage('Error fetching movies. Please try again later.');
     } finally {
-      setIsLoading(true)
+      setIsLoading(false)
     }
   }
 
+  const loadTrendingMovies = async () => {
+    try{
+      const movies = await getTrendingMovies();
+      setTrendingMovies(movies);
+    } catch(error) {
+      console.error(`Error fetching trending movies : ${error}`);
+    }
+  }
+
+  const scrollToAllMovies = () => {
+    window.scrollTo({
+      top: 1200,
+      behavior: 'smooth',
+    });
+  };
+
   useEffect(() => {
-    fetchMovies();
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm])
+
+  useEffect(() => {
+    loadTrendingMovies();
   }, [])
 
   return (
@@ -78,8 +114,29 @@ const App = () => {
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
 
-        <section className='all-movies'>
-          <h2 className='mt-[20px]'>All Movies</h2>
+
+
+        {trendingMovies.length > 0 && (
+          <section className='trending'>
+            <h2>
+              Trending Movies
+            </h2>
+
+            <ul>
+              {trendingMovies.map((movie, index) => (
+                <li key={movie.$id}>
+                  <p>
+                    {index + 1}
+                  </p>
+                  <img src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
+        <section className='all-movies' ref={allMoviesRef}>
+          <h2>All Movies</h2>
           {isLoading ? (
               <Spinner />
           ) : errorMessage ? (
@@ -89,9 +146,7 @@ const App = () => {
           ) : (
             <ul>
               {movieList.map((movie) => (
-                <p key={movie.id} className='text-white'>
-                  {movie.title}
-                </p>
+                <MovieCard key={movie.id} movie={movie} />
               ))}
             </ul>
           )}
